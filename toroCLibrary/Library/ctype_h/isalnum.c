a2fee43714a1403f157d0b9383a12edf7d289681
KilianKegel <kilian_kegel@hotmail.com>
2023-01-01 14:00:12 +0100

    update copyright

--- toroCLibrary/Library/ctype_h/isalnum.c
+++ toroCLibrary/Library/ctype_h/isalnum.c
@@ -3,7 +3,7 @@
     toro C Library
     https://github.com/KilianKegel/toro-C-Library#toro-c-library-formerly-known-as-torito-c-library
 
-    Copyright (c) 2017-2022, Kilian Kegel. All rights reserved.
+    Copyright (c) 2017-2023, Kilian Kegel. All rights reserved.
     SPDX-License-Identifier: GNU General Public License v3.0
 
 Module Name:
@@ -19,41 +19,6 @@ Author:
     Kilian Kegel
 
 --*/
-#include <ctype.h>
-/*
-Synopsis
-    include <ctype.h>
-    int isalnum(int c);
-Description
-    The isalnum function tests for any character for which isalpha or isdigit is true.
-
-    The function returns non-zero if c is any of:
-    a b c d e f g h i j k l m n o p q r s t u v w x y z
-    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-    0 1 2 3 4 5 6 7 8 9
-    or any other locale-specific alphabetic character.
-Parameter
-
-    @param[in] c : character to test
-
-Return
-
-    @return 0 : if no match
-    @return !0: otherwise
-*/
-/*
-    /*!
-\copyright
-
-    Torito C Library, Copyright (c) 2020, Kilian Kegel. All rights reserved.
-
-\mainpage isalnum
-
-\brief Implementation of the <em>isalnum</em> function
-
-\file isalnum.c
-
-*/
 #include <ctype.h>
 /*
 Synopsis
--- toroCLibrary/Library/string_h/StrTok.c
+++ toroCLibrary/Library/string_h/StrTok.c
@@ -3,7 +3,7 @@
     toro C Library
     https://github.com/KilianKegel/toro-C-Library#toro-c-library-formerly-known-as-torito-c-library
 
-    Copyright (c) 2017-2022, Kilian Kegel. All rights reserved.
+    Copyright (c) 2017-2023, Kilian Kegel. All rights reserved.
     SPDX-License-Identifier: GNU General Public License v3.0
 
 Module Name:
@@ -20,18 +20,6 @@ Author:
     Kilian Kegel
 
 --*/
-/*!
-\copyright
-
-    Torito C Library, Copyright (c) 2020, Kilian Kegel. All rights reserved.
-
-\mainpage StrTok
-
-\brief Implementation of the <em>StrTok</em> function
-
-\file StrTok.c
-
-*/
 #include <CdeServices.h>
 
 /**
